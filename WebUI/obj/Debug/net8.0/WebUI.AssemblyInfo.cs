ffe4751d1a84a34b4c9f8c04879b2ae87ada9b25
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-08-29 15:20:24 +0300

    ..

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44b1fbc41f88b49b913ec29ee1f4bb281ea1bb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2ca5c710579937a8410017bac7bee16e1aa2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
